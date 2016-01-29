0b2a0d812604cb33bf9476f43e02b20401a12bfe
Liu Zheng <liuzhengwd2005@163.com>
2016-01-29 15:02:43 +0800

    nimble

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yeomanWebApp
+ * @description
+ * # yeomanWebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yeomanWebApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
