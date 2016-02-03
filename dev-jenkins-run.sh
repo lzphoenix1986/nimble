22083224896050da4e05eb917adf96118481e037
Liu Zheng <liuzhengwd2005@163.com>
2016-02-03 16:04:10 +0800

    add jenkins modify

--- dev-jenkins-run.sh
+++ dev-jenkins-run.sh
@@ -19,5 +19,5 @@ npm install
 echo "grunt build"
 grunt build
 
-echo "grunt serve:dist >> /data/jenkins/workspace/Janna/jannaRun.log 2>&1 &"
-nohup grunt serve:dist >> /data/jenkins/workspace/Janna/jannaRun.log 2>&1 &
+echo "grunt serve:dist >> /root/.jenkins/workspace/Janna/jannaRun.log 2>&1 &"
+nohup grunt serve:dist >> /root/.jenkins/workspace/Janna/jannaRun.log 2>&1 &
